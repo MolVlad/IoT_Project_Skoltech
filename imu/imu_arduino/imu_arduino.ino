b628ca259c8e80ced3258e2cb63b068d5ca9ccbb
Dmitrii Masnyi <Dmitrii.Masnyi@skoltech.ru>
2021-12-05 17:21:20 +0500

    server ref

---
+++ imu/imu_arduino/imu_arduino.ino
@@ -0,0 +1,109 @@
+
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <Wire.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
+
+Adafruit_MPU6050 mpu;
+
+// WiFi network name and password:
+const char * networkName = "Masny"; // specify SSID here
+const char * networkPswd = "5sJn8eXw"; // specify password here
+
+//IP address to send UDP data to:
+// either use the ip address of the server or 
+// a network broadcast address
+const char * udpAddress = "192.168.0.103"; // specify server IP here
+const int udpPort = 60001; // specify server port here
+
+//Are we currently connected?
+boolean connected = false;
+
+//The udp library class
+WiFiUDP udp;
+
+void setup(){
+  // Initilize hardware serial:
+  Serial.begin(115200);
+  
+  //Connect to the WiFi network
+  connectToWiFi(networkName, networkPswd);
+
+  while (!Serial)
+    delay(10); // will pause Zero, Leonardo, etc until serial console opens
+  Serial.println("Adafruit MPU6050 test!");
+
+  // Try to initialize!
+  if (!mpu.begin()) {
+     Serial.println("Failed to find MPU6050 chip");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found!");
+
+  mpu.setAccelerometerRange(MPU6050_RANGE_2_G);
+
+  mpu.setGyroRange(MPU6050_RANGE_250_DEG);
+
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+  delay(100);
+  Serial.println("");
+  delay(100);
+}
+
+void loop(){
+  //only send data when connected
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+  if(connected){
+    //Send a packet
+    udp.beginPacket(udpAddress,udpPort);
+    udp.printf("%lf, %lf, %lf, %lf, %lf, %lf\n", 
+                              a.acceleration.x, a.acceleration.y, a.acceleration.z,
+                              g.gyro.x, g.gyro.y, g.gyro.z);
+
+    udp.endPacket();
+  }
+  Serial.printf("%lf, %lf, %lf, %lf, %lf, %lf\n", 
+                              a.acceleration.x, a.acceleration.y, a.acceleration.z,
+                              g.gyro.x, g.gyro.y, g.gyro.z);
+  delay(27);
+}
+
+void connectToWiFi(const char * ssid, const char * pwd){
+  Serial.println("Connecting to WiFi network: " + String(ssid));
+
+  // delete old config
+  WiFi.disconnect(true);
+  //register event handler
+  WiFi.onEvent(WiFiEvent);
+  
+  //Initiate connection
+  WiFi.begin(ssid, pwd);
+
+  Serial.println("Waiting for WIFI connection...");
+}
+//wifi event handler
+void WiFiEvent(WiFiEvent_t event){
+    switch(event) {
+      case ARDUINO_EVENT_WIFI_STA_GOT_IP:
+          //When connected set 
+          Serial.print("WiFi connected! IP address: ");
+          Serial.println(WiFi.localIP());  
+          //initializes the UDP state
+          //This initializes the transfer buffer
+          udp.begin(WiFi.localIP(),udpPort);
+          connected = true;
+          break;
+      case ARDUINO_EVENT_WIFI_STA_DISCONNECTED:
+          Serial.println("WiFi lost connection");
+          connected = false;
+          break;
+      default: break;
+    }
+}
--- imu/server.py
+++ imu/server.py
@@ -1,82 +1,61 @@
 import socket
 import numpy as np
+
 localIP     = "192.168.0.103" #specify your IP
 localPort   = 60001 # specify port
-bufferSize  = 1024
-# Create a datagram socket
-UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-# Bind to address and ip
-UDPServerSocket.bind((localIP, localPort))
+bufferSize  = 1024 #define buffer size
+
 LEFT_IMU = '192.168.0.108' #specify adress of the left imu
 RIGHT_IMU = '' #specify adress of the right imu
-left_imu_acc_x = []
-left_imu_acc_y = []
-left_imu_acc_z = []
-left_imu_gyro_x = []
-left_imu_gyro_y = []
-left_imu_gyro_z = []
-right_imu_acc_x = []
-right_imu_acc_y = []
-right_imu_acc_z = []
-right_imu_gyro_x = []
-right_imu_gyro_y = []
-right_imu_gyro_z = []
 
-left = [left_imu_acc_x, left_imu_acc_y, left_imu_acc_z, left_imu_gyro_x, left_imu_gyro_y, left_imu_gyro_z]
-right = [right_imu_acc_x, right_imu_acc_y, right_imu_acc_z, right_imu_gyro_x, right_imu_gyro_y, right_imu_gyro_z]
+left = []
+right = []
 
-left_imu_acc_x_avg = []
-left_imu_acc_y_avg = []
-left_imu_acc_z_avg = []
-left_imu_gyro_x_avg = []
-left_imu_gyro_y_avg = []
-left_imu_gyro_z_avg = []
-right_imu_acc_x_avg = []
-right_imu_acc_y_avg = []
-right_imu_acc_z_avg = []
-right_imu_gyro_x_avg = []
-right_imu_gyro_y_avg = []
-right_imu_gyro_z_avg = []
+left_avg = []
+right_avg = []
 
-left_avg = [left_imu_acc_x_avg, left_imu_acc_y_avg, left_imu_acc_z_avg, left_imu_gyro_x_avg, left_imu_gyro_y_avg, left_imu_gyro_z_avg]
-right_avg = [right_imu_acc_x_avg, right_imu_acc_y_avg, right_imu_acc_z_avg, right_imu_gyro_x_avg, right_imu_gyro_y_avg, right_imu_gyro_z_avg]
 
 def write_raw_data_to_queue(rec_data, imu_side):
     if rec_data is None or rec_data == []:
         return
     else:
         if imu_side == LEFT_IMU:
-            for index, queue in enumerate(left):
-                queue.append(abs(rec_data[index])) # append absolute value
+            left.append(np.abs(rec_data)) # append absolute value
         elif imu_side == RIGHT_IMU:
-            for index, queue in enumerate(right):
-                queue.append(abs(rec_data[index])) # append absolute value
+            right.append(np.abs(rec_data)) # append absolute value
+
 
 def preliminary_prepr(imu_side):
     if (left or right) is None or (left or right) == []:
         return
     else:
         if imu_side == LEFT_IMU:
-            for index, queue in enumerate(left):
-                if len(queue) < 36:
-                    break
-                else:
-                    popped_queue = [queue.pop(0) for i in range(36)] # 36 because of sampling rate (36 Hz)
-                    left_avg[index].append(np.mean(popped_queue)) # calculate avg value in 1 sec 
+            if len(left) < 36:
+                return
+            else:
+                popped_queue = [left.pop(0) for i in range(36)] # 36 because of sampling rate (36 Hz)
+                left_avg.append(np.mean(np.reshape(popped_queue, (36,6)), axis = 0)) # calculate avg value in 1 sec 
+                
         elif imu_side == RIGHT_IMU:
-            for index, queue in enumerate(right):
-                if len(queue) < 36:
-                    break
-                else:
-                    popped_queue = [queue.pop(0) for i in range(36)] # 36 because of sampling rate (36 Hz)
-                    right_avg[index].append(np.mean(popped_queue)) # calculate avg value in 1 sec 
-          
-while True:
-    bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
-    message = bytesAddressPair[0].decode('utf-8').replace('\n', '').split(',')
-    received_data = list(np.array(message, dtype = float))
-    address = bytesAddressPair[1]
-    write_raw_data_to_queue(received_data, address[0])
-    preliminary_prepr(address[0])
-    if left_imu_acc_x_avg != []: # just for debug
-        print(left_imu_acc_x_avg[-1])
+            if len(right) < 36:
+                return
+            else:
+                popped_queue = [right.pop(0) for i in range(36)] # 36 because of sampling rate (36 Hz)
+                right_avg.append(np.mean(np.reshape(popped_queue, (36,6)), axis = 0)) # calculate avg value in 1 sec 
+        
+def run_server():
+    UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM) #create socket
+    UDPServerSocket.bind((localIP, localPort)) # bind port and ip
+    while True:
+        bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
+        message = bytesAddressPair[0].decode('utf-8').replace('\n', '').split(',')
+        received_data = list(np.array(message, dtype = float))
+        address = bytesAddressPair[1]
+        write_raw_data_to_queue(received_data, address[0])
+        preliminary_prepr(address[0])
+        if left_avg != []: 
+            print(left_avg.pop(0)) # just for debug
+
+
+if __name__ == "__main__":
+    run_server()
